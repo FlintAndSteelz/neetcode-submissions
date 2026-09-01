79a22058726ae74dd059bec5ce8f9d594b6cff57
FlintAndSteelz <doantrongquy07@gmail.com>
2026-09-01 20:30:51 +0700

    Add: trapping-rain-water - submission-19

---
+++ Data Structures & Algorithms/trapping-rain-water/submission-19.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int trap(vector<int>& height) {
+        int n = height.size();
+        if (n<0) return 0;
+        int left = 0;
+        int right = n -1;
+        int leftMax = height[left];
+        int rightMax = height[right];
+        int result = 0;
+        while (left < right){
+            if (leftMax < rightMax){
+                left++;
+                leftMax = max(leftMax, height[left]);
+                result += leftMax - height[left];
+            }
+            else{
+                right--;
+                rightMax = max(rightMax, height[right]);
+                result += rightMax - height[right];
+            }
+        }
+        return result;
+    }
+};
